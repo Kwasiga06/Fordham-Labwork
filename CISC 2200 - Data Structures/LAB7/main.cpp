0bd6b80cb0a1eb4ed97f73488e8953516e2f6430
Kwasiga06 <kwasigyimahasante06@gmail.com>
2025-10-20 17:28:24 -0400

    Completed Lab7 Data Structures

--- CISC 2200 - Data Structures/LAB7/main.cpp
+++ CISC 2200 - Data Structures/LAB7/main.cpp
@@ -1,23 +1,3 @@
-/* 
-Based on the article (code in Java) in
-
-https://ics.uci.edu/~pattis/ICS-H22/lectures/llrecursion/
-
-To the LinkList class in linkList.h, add the following recursive functions:
-int recGetLength()
-void recReversePrint()
-
-Link * recSearchFor(int n)
-void recRemoveFirst(int n) // first occurrence of n if exists
-void recRemoveAll(int n)
-void recPrint()
-
-Note: Since recGetLength() takes no parameters, it needs to call another recursive method that takes a Link first as a parameter.
-You can overload method recGetLength() with another one recGetLength(Link lnk). So recGetLength() can call recGetLength(first).
-
-
-*/
-
 #include "linkList.h"
 using namespace std;
 
@@ -33,27 +13,25 @@ int main(){
     
     list.recPrint();
     cout << endl;
-    cout << "recGetLength(): " << endl;
+
+    cout << "recGetLength():" << endl;
     cout << list.recGetLength() << endl;
     cout << endl;
-    cout << "recReversePrint(): " << endl;
+
+    cout << "recReversePrint():" << endl;
     list.recReversePrint();
     cout << endl;
-    int searchNum;
-    cin >> searchNum;
-    cout << "recSearchFor(" << searchNum << "): " << endl;
+
+    int searchNum = 45;
     Link* found = list.recSearchFor(searchNum);
     if (found != NULL){
         found->displayLink();
         cout << "was found" << endl;}
-    else cout << searchNum <<" was not found" << endl;
+        else cout << searchNum <<" was not found" << endl;
     cout << endl;
-    list.recPrint();
-    int removeNum;
-    cin >> removeNum;
-    cout << "recRemoveFirst(" << removeNum << "):" << endl;
-    list.recRemoveFirst(removeNum);
-    // Try to find the removed value
+    list.recReversePrint();
+
+    int removeNum = 44;
     Link* foundAfter = list.recSearchFor(removeNum);
     if (foundAfter != NULL) {
         foundAfter->displayLink();
@@ -61,29 +39,18 @@ int main(){
     } else {
         cout << removeNum << " was not found" << endl;
     }
-    list.recReversePrint();
-    cout << endl;
+    list.recRemoveFirst(removeNum);
+    list.recPrint();
 
-    cout << "recRemoveAll(" << removeNum << "):" << endl;
-    list.recRemoveAll(removeNum);
-    // Try to find the removed value
-    Link* foundAllAfter = list.recSearchFor(removeNum);
-    if (foundAllAfter != NULL) {
-        foundAllAfter->displayLink();
+    Link* foundAfter2 = list.recSearchFor(removeNum);
+    if (foundAfter2 != NULL) {
+        foundAfter2->displayLink();
         cout << "was found" << endl;
     } else {
         cout << removeNum << " was not found" << endl;
     }
-    cout << endl;
-    list.recReversePrint();
-
-
-    
-   
-
-
-
-
+    list.recRemoveAll(removeNum);
+    list.recPrint();
 
     return 0;
 }
