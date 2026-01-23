055c03737d7da00608584c8d2a9f5d9c280c08f8
Kwasi <kwasigyimahasante06@gmail.com>
2026-01-23 11:07:40 -0500

    Finished the lab

--- CISC 4080 - Computer Algorithms/lab0.py
+++ CISC 4080 - Computer Algorithms/lab0.py
@@ -11,33 +11,96 @@ def RandomList(len):
       a.append (random_int)
    return a
 
+###############################
+# MY FUNCTIONS
+###############################
 
-# Find smallest element in a list
-# @param a: a list
-# @return largest value stored in a 
-# For example, if a[0…4]=[25,3, 7, 100]
-#     
-def Smallest (a, first=0, last=None): 
-    if last==None:
-       last = len(a)-1
+def SortedCheck(a):
+   """
+   Checks if the list is sorted in descending order.
+   Returns True if sorted in descending order.
+   """
+   for i in range(1, len(a)):
+      if a[i] > a[i - 1]:
+         return False
+   return True
 
-    smallest=math.inf
-    for i in range(first, last+1):
-      if a[i]<smallest:
-         smallest = a[i]
+def CheckDuplicate_Sorted(a):
+   """
+   Checks if a sorted list has any duplicate elements.
+   Returns True if there are duplicates.
+   """
+   for i in range(1, len(a)):
+      if a[i] == a[i - 1]:
+         return True
+   return False
 
-    return smallest 
+
+def CheckDUplicate_Unsorted(a):
+   """
+   Checks if an unsorted list has any duplicates.
+   Returns True if there are duplicates.
+   """
+   for i in range(len(a) - 1):
+      for j in range(i + 1, len(a)):
+         if a[i] == a[j]:
+            return True
+   return False
+
+def isPalindrome(word):
+   """
+   Checks if the string 'word' is a palindrome using slicing (coding pattern #4).
+   Returns True if palindrome.
+   """
+   return word == word[::-1]
 
 
-l=[-30,1,2,10,-1,-20]
-a=Smallest(l)
-print(a)
+###############################
+# TESTS FOR EACH FUNCTION
+###############################
 
-b=Smallest(l,3)
-print(b)
+# SORTED CHECK 
+print("Testing SortedCheck:")
+print(SortedCheck([10, 9, 8, 7, 6]))
+print(SortedCheck([1, 2, 3, 4, 5]))
+print(SortedCheck([5, 5, 3, 1]))
+print(SortedCheck([10]))
+print(SortedCheck([])) 
 
-b=Smallest(l,3,4)
-print(b)
+# Test with random list
+rand0list = RandomList(5)
+print(f"Random list: {rand0list}")
+print("Is sorted descending?", SortedCheck(rand0list))
 
-a=RandomList(10);
-print(a)
+# DUPLICATE SORTED
+print("\nTesting CheckDuplicate_Sorted:")
+print(CheckDuplicate_Sorted([9, 7, 7, 3, 1]))
+print(CheckDuplicate_Sorted([5, 4, 3, 2, 1]))
+print(CheckDuplicate_Sorted([5, 5, 5, 5]))
+print(CheckDuplicate_Sorted([10]))  
+print(CheckDuplicate_Sorted([]))
+
+
+# DUPLICATE UNSORTED
+print("\nTesting CheckDUplicate_Unsorted:")
+print(CheckDUplicate_Unsorted([1, 2, 3, 4, 5]))
+print(CheckDUplicate_Unsorted([1, 2, 3, 2, 5]))
+print(CheckDUplicate_Unsorted([5, 5, 5, 5]))
+print(CheckDUplicate_Unsorted([10]))  
+print(CheckDUplicate_Unsorted([])) 
+
+# Test with random list
+rand0list2 = RandomList(8)
+print(f"Random list: {rand0list2}")
+print("Has duplicates?", CheckDUplicate_Unsorted(rand0list2))
+
+# PALINDROME
+print("\nTesting isPalindrome:")
+print(isPalindrome('Welcome'))
+print(isPalindrome('kayak'))  
+print(isPalindrome('racecar'))
+print(isPalindrome('a'))
+print(isPalindrome('')) 
+print(isPalindrome('madam'))  
+print(isPalindrome('python')) 
+
